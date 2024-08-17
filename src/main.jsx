c14a5b05af390ea7791de6b91bb17f2a5c7960e3
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-08-17 13:54:27 +0600

    auth provider and toast message

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,13 @@ import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routers } from "./router";
 import "./index.css";
+import { AuthProvider } from "./providers/AuthProvider";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={routers} />
+  <AuthProvider>
+    <RouterProvider router={routers} />
+    <ToastContainer />
+  </AuthProvider>
 );
