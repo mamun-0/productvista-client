8b32df633d08c4d64b79495f0f286bdedeab1b69
mamun-0 <md.abdullahalmamun200@gmail.com>
2024-08-17 13:55:27 +0600

    auth provider

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,74 @@
+import { createContext, useEffect, useState } from "react";
+import { auth } from "../firebase/firebase.config";
+import {
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import axiosCommon from "../axios/axiosCommon";
+// Create Auth-Context
+export const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    setLoading(true);
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+
+  const signInWithGoogle = () => {
+    const googleProvider = new GoogleAuthProvider();
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const logOut = async () => {
+    localStorage.removeItem("access-token");
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      if (currentUser) {
+        // const userInfo = { email: currentUser.email };
+        // common.post("/jwt", userInfo).then((res) => {
+        //   const { token } = res.data;
+        //   if (token) {
+        //     localStorage.setItem("access-token", token);
+        //   } else {
+        //     localStorage.removeItem("access-token");
+        //   }
+        // });
+      }
+      setLoading(false);
+    });
+    return () => {
+      return unsubscribe();
+    };
+  }, []);
+
+  const authInfo = {
+    user,
+    setUser,
+    loading,
+    setLoading,
+    createUser,
+    signIn,
+    signInWithGoogle,
+    logOut,
+  };
+
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
+};
